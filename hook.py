b62dc4f158ab46b8fb4401bc88380360633e767b
OGURA_Daiki <8hachibee125@gmail.com>
2012-12-22 17:25:02 +0900

    make setting file parser split as modules is named configuration_parser.

--- hook.py
+++ hook.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from shutil import copyfile
+import re
 from os import path
 
 import requests
 import json
-
 
 """
 hook.py
@@ -15,28 +14,68 @@ hook.py
 install, description(info), original_path
 """
 
+def configuration_parser(line):
+    """ parse dotfile's configration and built Hook object. """
+    st = line.split(',')
+    uri = st[0]
+    hooktype = st[1]
+    description = st[2]
+
+    def build_hook():
+        # if there is only numbers, it is gist-id
+        #TODO 一旦完成してからもっとまともな実装を
+        r = re.compile('^\d+$')
+        if r.match(uri):
+            return GistHook(uri, hooktype)
+        else:
+            return LocalHook(uri, hooktype)
+    return build_hook()
+
+
 class Hook(object):
     """ abstract class. This work as hook contents. """
 
-    def __init__(self, install_dir):
-        self.install_dir = install_dir
+    # ひとまずこんだけ。ailiasとかはのちのち。もしくは分けるかも
+    hooktype_definitions = [
+            "pre-commit"
+            ,"prepare-commit-msg"
+            ,"commit-msg"
+            ,"post-commit"
+            ,"pre-rebase"
+            ,"post-rebase"
+            ,"post-merge"
+            ]
 
-    def install(self, contents_path):
-        #TODO あとで
+    def __init__(self, _contents_path, _hooktype):
+        self._contents_path = _contents_path
+        self._hooktype_save_location = self.__initialize_hook(_hooktype)
+
+    def __initialize_hook(self, hooktype):
+        # そのうちdictとかそういう系にするので
+        matchedhook = [h for h in self.hooktype_definitions if h == hooktype]
+        if matchedhook == None:
+            # TODO InvailedHookTypeExceptionとか
+            raise Exception
+        return matchedhook[0]
+
+    def install(self, git_hook_dir):
+        #TODO あとでNotImplementExceptionだか作る
         raise Exception()
 
     def _write(self, file_name, code):
-        with open(path.join(self.install_dir, file_name), 'w') as hook_file:
+        target_fullpath = path.join(self.install_dir,
+                          self._hooktype_save_location, file_name)
+        with open(target_fullpath, 'w') as hook_file:
             hook_file.write(code)
 
 
 class GistHook(Hook):
 
-    def __init__(self, install_dir):
-        Hook.__init__(self, install_dir)
+    def __init__(self, _contents_path, _hooktype):
+        Hook.__init__(self, _contents_path, _hooktype)
 
-    def install(self, contents_path):
-        gist = Gist(contents_path)
+    def install(self, git_hook_dir):
+        gist = Gist(self._contents_path)
         raw_url = gist.get_raw_url()
         print u'get from raw-url: %s\n' % raw_url
         print u'description: %s\n' % gist.get_description()
