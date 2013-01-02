f9c7c5dbe6dff5470a91dc87767c2f5d2944c8be
OGURA_Daiki <8hachibee125@gmail.com>
2013-01-02 16:59:55 +0900

    split hook timing resolve logic as class

--- hooks/hook.py
+++ hooks/hook.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-from os import path, mkdir, sep as path_separator
+from os import path, mkdir, sep as path_separator, chmod
 
 import requests
 import json
@@ -35,8 +35,8 @@ def configuration_parser(line):
     return build_hook()
 
 
-class Hook(object):
-    """ abstract class. This work as hook contents. """
+class HookTimingResolver(object):
+    """ judge the hooktype get from setting file is right. """
 
     # ひとまずこんだけ。ailiasとかはのちのち。もしくは分けるかも
     timing_definitions = [
@@ -49,18 +49,29 @@ class Hook(object):
             ,u"post-merge"
             ]
 
-    def __init__(self, _contents_path, _hooktype):
-        self._contents_path = _contents_path
-        self._hooktype_save_location = self.__initialize_hook(_hooktype)
+    def __init__(self, hooktiming):
+        self.hooktiming = self.__initialize_hook(hooktiming)
 
-    def __initialize_hook(self, hooktype):
+    def __initialize_hook(self, hooktiming):
         # そのうちdictとかそういう系にするので
-        matchedhook = [h for h in self.timing_definitions if h == hooktype]
+        matchedhook = [h for h in self.timing_definitions if h == hooktiming]
         if len(matchedhook) == 0:
-            print u"git has not hooktype such as " + hooktype
+            print u"git has not hooktype such as " + hooktiming
             # TODO InvailedHookTypeExceptionとか
             raise Exception
         return matchedhook[0]
+
+    def dir_name(self):
+        return self.hooktiming
+
+
+class Hook(object):
+    """ abstract class.abs.
+        This work as hook contents. """
+
+    def __init__(self, _contents_path, _hooktype):
+        self._contents_path = _contents_path
+        self._hooktype_save_location = HookTimingResolver(_hooktype).dir_name()
 
     def install(self, git_hook_dir):
         #TODO あとでNotImplementExceptionだか作る
